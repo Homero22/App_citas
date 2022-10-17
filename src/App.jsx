2c770adf012aacb07a6a6ff5ea9d3d17607de8c9
Homero <homero6834@gmail.com>
2022-10-17 12:39:33 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  const sumar = ()=>{console.log(2+2)}
+  sumar()
+  return (
+    <div className="App">
+      <h1>Hola Mundo</h1>
+      <input type="text" />
+    </div>
+  )
+}
+
+export default App
